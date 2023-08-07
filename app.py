804114b50936787d2523b6642fad24d7ef12d6fd
Nadhiar Pradana <nadhiar11@gmail.com>
2023-08-07 10:58:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,30 @@
-# Install the required libraries
-# !pip install transformers torch Flask streamlit
-
 import torch
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import BertTokenizer, BertForQuestionAnswering
 import streamlit as st
 
 def create_qa_model():
-    model_name = "cahya/bert-base-indonesian-tydiqa"
-    tokenizer = T5Tokenizer.from_pretrained(model_name)
-    model = T5ForConditionalGeneration.from_pretrained(model_name)
+    model_name = "indobenchmark/indobert-base-p2"
+    tokenizer = BertTokenizer.from_pretrained(model_name)
+    model = BertForQuestionAnswering.from_pretrained(model_name)
     return model, tokenizer
 
-def generate_answer(question):
+def generate_answer(question, context):
     model, tokenizer = create_qa_model()
-    input_text = "question: {} context: ".format(question)
+    input_text = "[CLS] " + question + " [SEP] " + context + " [SEP]"
     input_ids = tokenizer.encode(input_text, return_tensors="pt", max_length=512, truncation=True)
-    with torch.no_grad():
-        output = model.generate(input_ids)
-    answer = tokenizer.decode(output[0], skip_special_tokens=True)
+    start_scores, end_scores = model(input_ids)
+    start_index = torch.argmax(start_scores)
+    end_index = torch.argmax(end_scores) + 1
+    answer = tokenizer.decode(input_ids[0, start_index:end_index], skip_special_tokens=True)
     return answer
 
 def main():
-    st.title("Question & Answer System")
+    st.title("Question & Answer System (Bahasa Indonesia)")
     question = st.text_input("Masukkan Pertanyaan:")
+    context = st.text_area("Masukkan Konteks:")
     if st.button("Tanya"):
-        if question:
-            answer = generate_answer(question)
+        if question and context:
+            answer = generate_answer(question, context)
             st.subheader("Jawaban:")
             st.write(answer)
 
