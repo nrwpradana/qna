f2bbde44f8abf79f9ccf3d159199ba596c1f4bf6
Nadhiar Pradana <nadhiar11@gmail.com>
2023-08-07 11:08:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,46 @@
-import torch
-from transformers import BertTokenizer, BertForQuestionAnswering
 import streamlit as st
+import openai
 
-def create_qa_model():
-    model_name = "indobenchmark/indobert-base-p2"
-    tokenizer = BertTokenizer.from_pretrained(model_name)
-    model = BertForQuestionAnswering.from_pretrained(model_name)
-    return model, tokenizer
-
-def generate_answer(question, context):
-    model, tokenizer = create_qa_model()
-    input_text = "[CLS] " + question + " [SEP] " + context + " [SEP]"
-    input_ids = tokenizer.encode(input_text, return_tensors="pt", max_length=512, truncation=True)
-    start_scores, end_scores = model(input_ids)
-    start_index = torch.argmax(start_scores)
-    end_index = torch.argmax(end_scores)
-    answer = tokenizer.decode(input_ids[0, start_index:end_index+1], skip_special_tokens=True)
-    return answer
-
+# Streamlit App
 def main():
-    st.title("Question & Answer System (Bahasa Indonesia)")
-    question = st.text_input("Masukkan Pertanyaan:")
-    context = st.text_area("Masukkan Konteks:")
-    if st.button("Tanya"):
-        if question and context:
-            answer = generate_answer(question, context)
-            st.subheader("Jawaban:")
-            st.write(answer)
+    st.title("Sistem Question & Answer dengan GPT-3.5 Bahasa Indonesia")
+    
+    # Input API key
+    api_key = st.text_input("Masukkan API key OpenAI:", "")
+
+    if api_key:
+        openai.api_key = api_key
+        model_name = 'gpt-3.5-turbo'  # Model Bahasa Indonesia GPT-3.5
+
+        # Fungsi untuk mendapatkan jawaban dari pertanyaan menggunakan GPT-3.5
+        def get_answer(question, context):
+            prompt = f"pertanyaan: {question}\nkonteks: {context}\njawaban:"
+            response = openai.Completion.create(
+                engine=model_name,
+                prompt=prompt,
+                max_tokens=150,
+                stop=["\n"],
+                temperature=0.7,
+                n=1,
+            )
+            answer = response.choices[0].text.strip()
+            return answer
+
+        context = st.text_area(
+            "Masukkan teks konteks di sini:",
+            "Pisang adalah buah yang sangat enak dan bergizi. "
+            "Buah ini memiliki kulit kuning dan daging yang manis.",
+        )
+        question = st.text_input("Masukkan pertanyaan Anda di sini:", "Apa warna kulit pisang?")
+
+        if st.button("Jawab"):
+            if context and question:
+                answer = get_answer(question, context)
+                st.markdown(f"**Jawaban:** {answer}")
+            else:
+                st.warning("Masukkan konteks dan pertanyaan terlebih dahulu.")
+    else:
+        st.warning("Masukkan API key OpenAI terlebih dahulu.")
 
 if __name__ == "__main__":
     main()
